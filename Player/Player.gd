cac0a3adc8147ab542ecf254e99cbace53713b9f
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-05-21 18:18:42 -0400

    Dust tweaks
    - Dust looks alright when you walk down
    - Dust now frees itself when it's done

--- Player/Player.gd
+++ Player/Player.gd
@@ -128,7 +128,12 @@ func _input(event):
 func add_walk_particles(spawnPos:Vector2):
 	var newDust = walkParticles.instance()
 	newDust.position = spawnPos
-	newDust.scale.x = -vel.normalized().x if vel.normalized().x != 0 else 1
+	
+	# Making it look right
+	var normalizedVelX = vel.normalized().x
+	if normalizedVelX != 0: newDust.scale.x = -normalizedVelX
+	else: newDust.rotation_degrees = 300
+	
 	newDust.emitting = true
 	add_child(newDust)
 
