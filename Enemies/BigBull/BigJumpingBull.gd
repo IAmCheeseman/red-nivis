25f73f58dcf53074e490dbb6a1b9e75f1fc0be5f
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-05-21 10:35:30 -0400

    Behavior
    Made enemy circle around the player

--- Enemies/BigBull/BigJumpingBull.gd
+++ Enemies/BigBull/BigJumpingBull.gd
@@ -18,6 +18,8 @@ export var speed := 850
 export var maxHealth := 4
 export var sightDist := 16*6
 export var acceleration = 2.0
+export var jumpDist = 120
+export var rotSpeed = 10
 export var friction := 1.5
 
 onready var sightLine = $Sight
@@ -32,10 +34,10 @@ onready var softCollision = $SoftCollision
 var player : KinematicBody2D
 var world
 var vel = Vector2.ZERO
-var target
 var lastFramePos = Vector2.ZERO
 var state = WANDER setget set_state
 var futurePosition = position
+var targetPosRot = 0
 
 var tiles : TileMap
 
@@ -71,20 +73,10 @@ func _physics_process(delta):
 	AI(delta)
 
 
-
-func jump():
-	var randomness = Vector2(rand_range(-32, 32), rand_range(-32, 32))
-	var dir = global_position.direction_to(player.global_position+randomness)*speed
-	if rand_range(0, 5) <= 1:
-		dir = -dir
-	vel = dir
-
-
 func set_state(value):
 	state = value
 	if value == BOUNCE:
-		target = player.global_position
-		vel = global_position.direction_to(target)*(speed*1.4)
+		vel = global_position.direction_to(player.global_position)*(speed*2)
 
 
 func AI(delta):
@@ -97,12 +89,12 @@ func AI(delta):
 		WANDER:
 			wanderState(delta)
 		ATTACK:
-			attackState(delta)
+			attackState()
 		BOUNCE:
-			bounceState(delta)
+			bounceState()
 
 
-func bounceState(_delta):
+func bounceState():
 	animationPlayer.play("Roll")
 	shadow.global_position = sprite.global_position
 	shadow.global_position.y -= sprite.position.y*2
@@ -112,7 +104,7 @@ func bounceState(_delta):
 		stateTimer.start(stateTimes[0])
 
 
-func attackState(_delta):
+func attackState():
 	animationPlayer.play("Run")
 	sprite.scale.x = -1 if vel.x > 0 else 1
 
@@ -123,6 +115,16 @@ func attackState(_delta):
 	var canSeePlayer = !sightLine.is_colliding()
 	if !canSeePlayer:
 		state = WANDER
+
+
+func jump():
+	# Getting the position to jump towards
+	var targetPos = Vector2.RIGHT.rotated(deg2rad(targetPosRot))*jumpDist
+	targetPosRot += rotSpeed
+	targetPos += player.global_position
+
+	var jumpDir = global_position.direction_to(targetPos)
+	vel = jumpDir*speed
 
 
 
@@ -150,12 +152,5 @@ func _on_StateTimer_timeout():
 	state = BOUNCE if state == ATTACK else ATTACK
 	stateTimes.shuffle()
 	stateTimer.start(stateTimes[0])
-
-
-
-
-
-
-
 
 
