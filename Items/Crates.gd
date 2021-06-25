291bc161d98f8485cca4048f3f2bebfd2197e20e
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-06-25 07:53:11 -0400

    Made the crates have shadows

--- Items/Crates.gd
+++ Items/Crates.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+onready var shadow = $Shadow
+onready var sprite = $Sprite
+
 var crates = [
 	"SingleCrates",
 	"MultipleCrates"
@@ -14,3 +17,10 @@ func _ready():
 	var crateNum = get_node(crates[0])
 	var keys = Array(crateNum.get_animation_list())
 	crateNum.play(keys[rand_range(0, keys.size())])
+
+
+func _process(delta):
+	shadow.region_rect = sprite.region_rect
+	shadow.position.y = sprite.region_rect.size.y/2
+	print(sprite.region_rect.size)
+	set_process(false)
