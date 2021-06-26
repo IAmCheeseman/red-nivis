1ae210c6a944b69f387aeaf3ad37f647647d9015
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-06-26 14:39:08 -0400

    did iventory stuf

--- UI/Inventory/Inventory.gd
+++ UI/Inventory/Inventory.gd
@@ -6,7 +6,7 @@ export var maxSlots = 8
 var items = []
 
 # Item map
-var itemMapR = preload("res://UI/Inventory/ItemMap.gd")
+var itemMapR = preload("res://UI/Inventory/ItemMap.tres")
 var itemMap = itemMapR.items
 
 # Signals
@@ -14,31 +14,82 @@ signal itemsChanged
 signal itemDeleted
 signal itemAdded
 
+
+# Sorting class
+class sortByAmount:
+	static func sort_descending(a, b):
+		if a.amount > b.amount:
+			return true
+		return false
+
+
 # So I can make sure the item actually exists before doing anything.
-func check_if_exists(item:String) -> bool:
-	if itemMap.has(item):
+func check_if_exists(id:String) -> bool:
+	if itemMap.has(id):
 		return true
 	return false
 
 
-func add_item(item:String, amount:int):
+func filter_items(id:String):
+	var filteredItems = []
+	for item in items:
+		if item.id == id:
+			filteredItems.append(item)
+	return filteredItems
+
+
+func add_item(id:String, amount:int):
 	# Checking if the item can actually be added.
 	if items.size() > maxSlots:
 		return
-	if !check_if_exists(item):
-		push_error("ITEM DOES NOT EXIST: %s" % item)
+	if !check_if_exists(id):
+		push_error("ITEM DOES NOT EXIST: %s" % id)
 		return
-	
-	# Adding the item.
-	items.append({
-		"id" : item,
-		"amount" : amount
-    })
+	var maxStackSize = itemMap[id].maxStackSize
+
+	# Add items/stacks until there's none left.
+	var filteredItems = filter_items(id)
+
+	if filteredItems.size() != 0:
+		for slot in filteredItems:
+			if amount <= 0:
+				break
+			var amountRemaining = maxStackSize-slot.amount
+			slot.amount = min(slot.amount+amount, maxStackSize)
+			amount -= amountRemaining
+
+	while amount > 0:
+		items.append({
+			"id" : id,
+			"amount" : min(amount, maxStackSize)
+		})
+		amount -= maxStackSize
 
 	emit_signal("itemsChanged")
-	emit_signal("itemAdded", item)
+	emit_signal("itemAdded", id)
 
 
-func destroy_item(item:String, amount:int):
-	emit_signal("itemDeleted", item)
+func destroy_item(id:String, amount:int) -> bool:
+	# Filtering out all the items I don't need
+	var filteredItems = filter_items(id)
+	# Stopping if there's no items to destroy
+	if filteredItems.size() == 0:
+		return false
+
+	# Sorting the items by the amount from highest to lowest
+	# Then going through and removing as many stacks as I need.
+	filteredItems.sort_custom(sortByAmount, "sort_descending")
+
+	while amount != 0 :
+		var selectedStack = filteredItems.pop_front()
+		var amountRemaining = amount - selectedStack.amount
+		selectedStack.amount -= amount
+		if selectedStack.amount <= 0:
+			items.remove(selectedStack)
+		amount = amountRemaining
+
+	# Finishing up
+	emit_signal("itemsChanged")
+	emit_signal("itemDeleted", id)
+	return true
 
--- UI/Inventory/InventoryUI.gd
+++ UI/Inventory/InventoryUI.gd
@@ -10,9 +10,12 @@ func _ready():
 	inventory.connect("itemsChanged", self, "refresh_items")
 
 
-func format_name(iName:String) -> String:
-	var formattedName = iName
-	formattedName.format(" ", "_")
+func format_id(id:String) -> String:
+	var formattedName = id
+	# Replacing underscores with spaces
+	while formattedName.find("_"):
+		formattedName.replace("_", " ")
+	# Removing text between bars
 	var startHidden = formattedName.find("|")
 	var endHidden = formattedName.find_last("|")
 	var i = startHidden
@@ -20,12 +23,27 @@ func format_name(iName:String) -> String:
 		formattedName.replace(formattedName[i], "")
 		i += 1
 
+	# Finishing up
+	print(formattedName)
 	return formattedName
 
 
 func refresh_items():
 	for c in slots.get_children():
 		c.queue_free()
-	
+
 	for i in inventory.items:
-		pass
+		var newSlot = slot.instance()
+		slots.add_child(newSlot)
+		var itemName = format_id(i.id)
+		newSlot.setup(itemName, i.amount)
+
+
+func _input(event):
+	if Input.is_key_pressed(KEY_P):
+		var items_ = [
+			"cheese",
+			"health_potion"
+		]
+		items_.shuffle()
+		inventory.add_item(items_.pop_front(), 1)
