2a7766d7acd62f064e70ca0811394fb155db41fb
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-07-09 15:18:05 -0400

    Implemented world gen for some reason

--- WorldGeneration/World.gd
+++ WorldGeneration/World.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var tiles
 var backgroundTiles
-var shadows
 var planet:Planet
 onready var background = $BackgroundHelper/BackGround
 onready var player = $Props/Player
@@ -57,145 +56,31 @@ func generate_room():
 
 	planet = load("res://WorldGeneration/Planets/%s.tres" % planets[GameManager.planet])
 
-	# Getting the room image
-	var world:Image
-	var worldPos = worldManager.worldData.position
-	var roomImage = worldManager.worldData.rooms[worldPos.y][worldPos.x].layout
-	if !roomImage:
-		var worldGenerator = WorldGenerator.new()
-		worldManager.worldData.rooms[worldPos.y][worldPos.x].layout = worldGenerator.generate_room(planet)
-	roomImage = worldManager.worldData.rooms[worldPos.y][worldPos.x].layout
-	world = roomImage.duplicate()
-
-	world.lock()
-
-	# Setting up the tiles, enemies, & ruins
-
-	var enemyColor = Color("#a00c0c")
-	var solidColor = Color("#000000")
-	var ruinColor  = Color("#44cb30")
+	var worldGenerator = WorldGenerator.new()
+	var world = worldGenerator.generate_world(
+																	  # NOISE
+		load("res://WorldGeneration/Noise/HeightMap.tres"),           # Height
+		load("res://WorldGeneration/Noise/HeightMap.tres"),           # Shifting
+		load("res://WorldGeneration/Noise/CaveNoise.tres"),           # Caves
+																	  # CURVES
+		load("res://WorldGeneration/Curves/HeightSmoothing.tres"),    # Height
+		load("res://WorldGeneration/Curves/OverHangeSmoothing.tres"), # Shifting
+		load("res://WorldGeneration/Curves/CaveSize.tres")            # Caves
+		)
 
 	tiles = planet.solidTiles.instance()
 	props.add_child(tiles)
 
-	for x in world.get_width():
-		for y in world.get_height():
+	for x in world.size():
+		for y in world[x].size():
 			# Adding tiles
-			if world.get_pixel(x, y).is_equal_approx(solidColor):
-				tiles.set_cell(x, y, 0)
-				tiles.update_bitmask_area(Vector2(x, y))
-			# Enemies
-			elif world.get_pixel(x, y).is_equal_approx(enemyColor):
-				var enemies = planet.enemies
-				var enemyChances = planet.enemyChances
-				var weaponInstead = rand_range(0, 1) < .3333333
-
-				if enemies.size() > 0 and !weaponInstead:
-					var selectedEnemy = rand_range(0, enemies.size())
-					while rand_range(0, 100) > enemyChances[selectedEnemy]:
-						selectedEnemy = rand_range(0, enemies.size())
-
-					var enemy = enemies[selectedEnemy].instance()
-					enemy.position = Vector2(x, y)*16
-					enemy.set_player(player)
-					props.add_child(enemy)
-				elif weaponInstead:
-					var itemMap = ItemMap.new()
-					itemMap.weapons.shuffle()
-					var weaponPath = "res://Items/Weapons/WeaponScenes/%s.tscn" % itemMap.weapons[0]
-					var weapon = load(weaponPath).instance()
-					weapon.position = Vector2(x, y)*16
-					props.add_child(weapon)
-
-			# Ruins
-			elif check_approx(world.get_pixel(x, y),ruinColor):
-				var ruinCount = 4
-				var newRuin = load("res://WorldGeneration/Ruins/Exterior/BaseRuins%s.tscn" % int(rand_range(1, ruinCount+1))).instance()
-				newRuin.position = Vector2(x, y)*16
-				props.add_child(newRuin)
-
-	var playerOutput = worldData.lastUpdatedDir
-
-	# Placing the player and adding padding
-
-	# Adding an extra layer to the x axis
-	for x in world.get_width():
-		if tiles.get_cell(x, 0) != -1:
-			tiles.set_cell(x, -1, 0)
-			tiles.update_bitmask_area(Vector2(x, -1))
-		elif playerOutput == Vector2.DOWN:
-			player.position = Vector2(x, 1)
-		if tiles.get_cell(x, world.get_height()-1) != -1:
-			tiles.set_cell(x, world.get_height(), 0)
-			tiles.update_bitmask_area(Vector2(x, world.get_height()))
-		elif playerOutput == Vector2.UP:
-			player.position = Vector2(x, world.get_height()-1)
-	# Adding an extra layer to the y axis
-	for y in world.get_height():
-		if tiles.get_cell(0, y) != -1:
-			tiles.set_cell(-1, y, 0)
-			tiles.update_bitmask_area(Vector2(-1, y))
-		elif playerOutput == Vector2.RIGHT:
-			player.position = Vector2(1, y)
-		if tiles.get_cell(world.get_width()-1, y) != -1:
-			tiles.set_cell(world.get_width(), y, 0)
-			tiles.update_bitmask_area(Vector2(world.get_width(), y))
-		elif playerOutput == Vector2.LEFT:
-			player.position = Vector2(world.get_width()-1, y)
-	player.position *= tiles.cell_size.x
-
-	# Shadows
-	shadows = tiles.duplicate()
-	shadows.position.y += 12
-	shadows.modulate = Color(0, 0, 0, .5)
-	shadows.show_behind_parent = true
-	shadows.cell_y_sort = false
-	shadows.collision_layer = 0
-
-	tiles.add_child(shadows)
+			tiles.set_cell(x, y, world[x][y])
+			tiles.update_bitmask_area(Vector2(x, y))
 
 	# Setting the background
 	background.rect_position = Vector2.ONE*-20000
 	background.rect_size = Vector2(10000, 10000)*3
 	background.texture = planet.backgroundImage
-
-	# Setting the exit areas
-
-	# Left Exit Area
-	var shape = RectangleShape2D.new()
-# warning-ignore:integer_division
-	shape.extents = Vector2(2, world.get_height()/2*16)
-# warning-ignore:integer_division
-	setExitArea(Vector2(0, world.get_height()/2), shape, Vector2.LEFT)
-	# Right Exit Area
-	shape = RectangleShape2D.new()
-# warning-ignore:integer_division
-	shape.extents = Vector2(2, world.get_height()/2*16)
-# warning-ignore:integer_division
-	setExitArea(Vector2(world.get_width(), world.get_height()/2), shape, Vector2.RIGHT)
-	# Up Exit Area
-	shape = RectangleShape2D.new()
-# warning-ignore:integer_division
-	shape.extents = Vector2(world.get_width()/2*16, 2)
-# warning-ignore:integer_division
-	setExitArea(Vector2(world.get_width()/2, 0), shape, Vector2.UP)
-	# Down Exit Area
-	shape = RectangleShape2D.new()
-# warning-ignore:integer_division
-	shape.extents = Vector2(world.get_width()/2*16, 2)
-# warning-ignore:integer_division
-	setExitArea(Vector2(world.get_width()/2, world.get_height()), shape, Vector2.DOWN)
-
-	set_minimap(world)
-
-
-func setExitArea(_position, shape, dir):
-	var newExitArea = preload("res://WorldGeneration/ExitArea.tscn").instance()
-	newExitArea.position = _position*16
-	newExitArea.direction = dir
-	add_child(newExitArea)
-	newExitArea.set_collider(shape)
-	newExitArea.connect("move_to_room", worldManager, "move_in_direction")
 
 
 func check_approx(color:Color, color2:Color):
@@ -250,17 +135,12 @@ func _on_player_removeTile(mousePosition):
 
 		placementTiles.set_cellv(mapMousePos, 0)
 		placementTiles.update_bitmask_area(mapMousePos)
-
-		shadows.set_cellv(mapMousePos, 0)
-		shadows.update_bitmask_area(mapMousePos)
 		return
 
 	tiles.set_cellv(mapMousePos, -1)
 	tiles.update_bitmask_area(mapMousePos)
 	placementTiles.set_cellv(mapMousePos, -1)
 	placementTiles.update_bitmask_area(mapMousePos)
-	shadows.set_cellv(mapMousePos, -1)
-	shadows.update_bitmask_area(mapMousePos)
 
 
 func _on_World_tree_entered():
--- WorldGeneration/WorldGenerater.gd
+++ WorldGeneration/WorldGenerater.gd
@@ -1,136 +1,116 @@
-extends Node
+extends Resource
 class_name WorldGenerator
 
+export var worldSize:Vector2 = Vector2(1000, 700)
+export var strength:float = 5
+export var horizen:int = 500
+export var roughness:int = 1
+export var tileSize:int = 8
+export var shiftDirection:Vector2 = Vector2.LEFT
+export var shiftStrength:int = 5
+var heightSmoothing:Curve
+var overHangSmoothing:Curve
+var caveSize:Curve
 
-var minPrefabs = 2
-var maxPrefabs = 4
+var heightNoise
+var shiftNoise
+var caveNoise
 
-
-enum {NORTH, SOUTH, EAST, WEST, NONE}
-
-
-# Colors telling the algorithim what to place in certain spots
-var enemyColor = Color("#a00c0c")
-var emptyColor = Color("#ffffff")
-var solidColor = Color("#000000")
-var ruinColor = Color("#44cb30")
-
-# Directional colors
-var northColor = Color("#221f3e")
-var southColor = Color("#b14926")
-var eastColor  = Color("#490a15")
-var westColor  = Color("#174646")
+var map := []
 
 
-func generate_room(planet):
-	var prefabSize = 16
-	var prefabCount = 56
-	var outlineCount = 20
-	var viablePrefabs = []
-	for i in prefabCount:
-		viablePrefabs.append(i+1)
+func generate_world(
+	_heightNoise, _shiftNoise, _caveNoise,
+	_heightSmoothing, _overHangSmoothing, _caveSize):
+	randomize()
+	heightNoise = _heightNoise.noise
+	shiftNoise = _shiftNoise.noise
+	caveNoise = _caveNoise.noise
 
-	var roomOutlines = "res://WorldGeneration/RoomOutlines/RO%s.png"
-	var prefabs = planet.prefabs+"prefab%s.png"
+	heightSmoothing = _heightSmoothing
+	overHangSmoothing = _overHangSmoothing
+	caveSize = _caveSize
 
-	# Getting an image to be used as a general layout
-	var roomOutline:Image = load(
-		roomOutlines % round(rand_range(1, outlineCount))
-		).get_data()
+	heightNoise.seed = randi()
+	shiftNoise.seed = randi()
+	caveNoise.seed = randi()
 
-	# Creating the image used to hold the specific details of the room
-	var roomLayout = Image.new()
-	roomLayout.create(
-	(roomOutline.get_width()*prefabSize), (roomOutline.get_height()*prefabSize),
-	false, Image.FORMAT_RGBAH
-	)
+	# Creating the world
+	create_world_map()
+	add_height()
+	for x in map.size():
+		for y in map[x].size():
+			shift_tiles(x, y)
+			add_caves(x, y)
+	clean_up()
 
-	var roomRect = Rect2(0, 0, roomOutline.get_width(), roomOutline.get_height())
-
-	roomLayout.fill(solidColor)
-
-	roomOutline.lock()
-	roomLayout.lock()
-
-	var connectionMap = []
-
-	# Creating a connection map
-	for x in roomOutline.get_width():
-		connectionMap.append([])
-		for y in roomOutline.get_height():
-			var roomConnections = {
-# warning-ignore:narrowing_conversion
-				"north" : !roomOutline.get_pixel(x, clamp(y-1, 0, INF)).is_equal_approx(solidColor) and roomRect.has_point(Vector2(x, y-1)),
-# warning-ignore:narrowing_conversion
-				"south" : !roomOutline.get_pixel(x, clamp(y+1, -INF, roomOutline.get_height()-1) ).is_equal_approx(solidColor) and roomRect.has_point(Vector2(x, y+1)),
-# warning-ignore:narrowing_conversion
-				"west" : !roomOutline.get_pixel(clamp(x-1, 0, INF), y).is_equal_approx(solidColor) and roomRect.has_point(Vector2(x-1, y)),
-# warning-ignore:narrowing_conversion
-				"east" : !roomOutline.get_pixel(clamp(x+1, -INF, roomOutline.get_width()-1), y).is_equal_approx(solidColor) and roomRect.has_point(Vector2(x+1, y))
-			}
-
-			connectionMap[x].append(roomConnections)
-
-	# Creating the room layout
-	for x in roomOutline.get_width():
-		for y in roomOutline.get_height():
-
-			# Getting the prefab section color
-			var rLayoutP = roomOutline.get_pixel(x, y)
-
-			if !rLayoutP.is_equal_approx(solidColor):
-				# Otherwise, select a prefab to use
-				viablePrefabs.shuffle()
-				var prefab:Image = load(prefabs % viablePrefabs.pop_front()).get_data()
-				prefab.lock()
-
-				var exitDirection = get_dir(rLayoutP)
-
-				for xx in prefab.get_width():
-					for yy in prefab.get_height():
-						# Creating the prefab in the room
-						var tileColor = prefab.get_pixel(xx, yy)
-						var direction = get_dir(tileColor)
-
-						# Checking for ground
-						if tileColor.is_equal_approx(emptyColor):
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), emptyColor)
-						# Checking for an enemy
-						elif tileColor.is_equal_approx(ruinColor):
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), ruinColor)
-						# Checking for an enemy
-						elif tileColor.is_equal_approx(enemyColor):
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), enemyColor)
-
-						# Checking the directions
-						elif direction == NORTH and (connectionMap[x][y].north or exitDirection == NORTH): # NORTH
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), emptyColor)
-						elif direction == SOUTH and (connectionMap[x][y].south or exitDirection == SOUTH): # SOUTH
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), emptyColor)
-						elif direction == EAST and (connectionMap[x][y].east or exitDirection == EAST): # EAST
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), emptyColor)
-						elif direction == WEST and (connectionMap[x][y].west or exitDirection == WEST): # WEST
-							roomLayout.set_pixel(xx+(x*prefabSize), yy+(y*prefabSize), emptyColor)
-
-	var ca = CellularAutomata.new()
-	roomLayout = ca.iterate(roomLayout, planet.CAIterations, planet.CAStarveLimit, planet.CAOverPop, solidColor)
-	ca.queue_free()
-
-	roomLayout.save_png("user://output.png")
-
-	return roomLayout
+	return map;
 
 
-func get_dir(color:Color):
-	if color.is_equal_approx(northColor):
-		return NORTH
-	if color.is_equal_approx(southColor):
-		return SOUTH
-	if color.is_equal_approx(eastColor):
-		return EAST
-	if color.is_equal_approx(westColor):
-		return WEST
-	return NONE
+func create_world_map():
+	for x in worldSize.x:
+		map.append([])
+		for y in worldSize.y:
+			map[x].append(-1)
+
+
+func add_height():
+	for x in worldSize.x:
+		# Getting the height of the current column.
+		var columnHeight = clamp(horizen-round(( (heightNoise.get_noise_1d(x)*tileSize)*strength)), 0, worldSize.y)
+		columnHeight -= heightSmoothing.interpolate(columnHeight)
+		columnHeight -= ceil(rand_range(0, roughness))
+		var offset = worldSize.y-columnHeight
+		# Creating the column.
+		for y in columnHeight:
+			map[x][y+offset] = 0
+
+
+func shift_tiles(x, y):
+	pass
+
+
+func add_caves(x, y):
+	var threshold = caveSize.interpolate(y/worldSize.y)
+	var caveValue = caveNoise.get_noise_2d(x, y)
+	if caveValue > threshold:
+		map[x][y] = -1
+
+
+func clean_up():
+	var kill = 3
+	var iterations = 2
+
+	for i in iterations:
+		var changes = []
+		for x in worldSize.x:
+			for y in worldSize.y:
+				var neighbors = get_neighbors(Vector2(x, y))
+				var aliveCells = 0
+				for cell in neighbors:
+					if map[cell.x-1][cell.y-1] == -1:
+						aliveCells += 1
+				if aliveCells >= kill:
+					changes.append({
+						"x" : x,
+						"y" : y,
+						"val" : -1
+					})
+		for change in changes:
+			map[change.x][change.y] = change.val
+
+
+func get_neighbors(pos:Vector2):
+	return [
+		pos+Vector2.LEFT,
+		pos+Vector2.RIGHT,
+		pos+Vector2.UP,
+		pos+Vector2.DOWN,
+		pos+(Vector2.LEFT+Vector2.UP),
+		pos+(Vector2.RIGHT+Vector2.UP),
+		pos+(Vector2.LEFT+Vector2.DOWN),
+		pos+(Vector2.RIGHT+Vector2.DOWN)
+	]
 
 
 
