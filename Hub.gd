8d8be48cfa0b104b56f8b75815bacd17ca2ddb68
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-07-15 12:30:51 -0400

    the things work

---
+++ Hub.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Testing
+	var item = GameManager.itemManager.create_item("Cheese")
+	item.position = Vector2(95, 375)
+	add_child(item)
--- Items/Weapons/Bullet/BulletManagement.gd
+++ Items/Weapons/Bullet/BulletManagement.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 func _ready():
 	GameManager.spawnManager = self
-	# Testing
-	var item = GameManager.itemManager.create_item("Cheese")
-	item.position = position
-	add_child(item)
-
 
 func add_bullet(bullet:Node2D):
 	add_child(bullet)
