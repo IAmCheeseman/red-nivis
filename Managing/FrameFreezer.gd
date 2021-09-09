29a0bebfe6a978266f5211eb07f2dd845c69200e
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-09-09 11:20:06 -0400

    Removed some text because translation is hard

--- Managing/FrameFreezer.gd
+++ Managing/FrameFreezer.gd
@@ -7,7 +7,7 @@ var currentTimerCount = 0
 
 func freeze_frames(time:float) -> void:
 	var timer:Timer = Timer.new()
-	timer.connect("timeout", self, "_timer_done", [timer])
+	var _ok = timer.connect("timeout", self, "_timer_done", [timer])
 	timer.pause_mode = Node.PAUSE_MODE_PROCESS
 	
 	GameManager.add_child(timer)
