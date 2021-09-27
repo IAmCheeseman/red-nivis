5295a896163705d36016dd98a1106d4a841aea0e
Xander <cheeseman085@gmail.com>
2021-09-27 18:40:11 -0400

    Set up constants

--- Node2D.gd
+++ Node2D.gd
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
-	var image = RoomGenerator.generate(randi(), "res://World/Templates/LabTemplates/", 3, self)
+	var image = RoomGenerator.generate(randi(), "res://World/Templates/LabTemplates/", 11, self)
 	var tex = ImageTexture.new()
 	tex.create_from_image(image)
 	$Sprite.texture = tex
--- World/WorldManagement/RoomGenerator.gd
+++ World/WorldManagement/RoomGenerator.gd
@@ -1,19 +1,43 @@
 extends Node
 class_name RoomGenerator
 
-const TEMPLATE_SIZE = 16
+const TEMPLATE_SIZE = 10
+const TEMPLATE_PATH = "res://World/Templates/"
+
+const EMPTY = Color("#ffffff")
+const TILE = Color("#000000")
+const UP = Color("#5bb031")
+const DOWN = Color("#e089e0")
+const RIGHT = Color("#75d1e4")
+const LEFT = Color("#952008")
 
 
-static func generate(seed_:int, path:String, templateAmount:int, node:Node) -> Image:
+static func generate(seed_:int, templates:String, templateAmount:int, node:Node) -> Image:
 	seed(seed_)
 	var size:Vector2 = Vector2(rand_range(1, 3), rand_range(1, 3)).round()
 	var image:Image = Image.new()
 	image.create(size.x*TEMPLATE_SIZE, size.y*TEMPLATE_SIZE, true, Image.FORMAT_RGBA8)
 	
-	var templatePath = path+"Templates.png"
+	var templatePath = TEMPLATE_PATH+templates
 	
 	
 	return image
-	
-	
+
+
+static func get_random_template(template:StreamTexture, templateAmount:int):
+	var image := template.get_data()
+	return {
+		"solids" : image.get_rect(
+			Rect2(
+				Vector2(rand_range(0, image.get_width()/templateAmount), 0),
+				Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
+			)
+		),
+		"platforms" : image.get_rect(
+			Rect2(
+				Vector2(rand_range(0, image.get_width()/templateAmount), TEMPLATE_SIZE),
+				Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
+			)
+		)
+	}
 
