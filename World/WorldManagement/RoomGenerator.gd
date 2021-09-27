3c13364171a4321409470773ba697d2be9321e7e
Xander <cheeseman085@gmail.com>
2021-09-27 18:55:41 -0400

    should work but doesnt

--- World/WorldManagement/RoomGenerator.gd
+++ World/WorldManagement/RoomGenerator.gd
@@ -12,32 +12,37 @@ const RIGHT = Color("#75d1e4")
 const LEFT = Color("#952008")
 
 
-static func generate(seed_:int, templates:String, templateAmount:int, node:Node) -> Image:
+static func generate(seed_:int, templatesName:String, templateAmount:int) -> Image:
 	seed(seed_)
-	var size:Vector2 = Vector2(rand_range(1, 3), rand_range(1, 3)).round()
+	var size:Vector2 = Vector2(rand_range(2, 2), rand_range(2, 2)).round()
 	var image:Image = Image.new()
-	image.create(size.x*TEMPLATE_SIZE, size.y*TEMPLATE_SIZE, true, Image.FORMAT_RGBA8)
+	image.create(int(size.x*TEMPLATE_SIZE), int(size.y*TEMPLATE_SIZE), true, Image.FORMAT_RGBA8)
 	
-	var templatePath = TEMPLATE_PATH+templates
+	var templates = load(TEMPLATE_PATH+templatesName)
 	
+	image.lock()
+	for x in size.x:
+		for y in size.y:
+			var template = get_random_template(templates, templateAmount)
+			image.blit_rect(image, template.solids, Vector2(x*TEMPLATE_SIZE, y*TEMPLATE_SIZE))
+	image.unlock()
 	
 	return image
 
 
-static func get_random_template(template:StreamTexture, templateAmount:int):
+static func get_random_template(template:StreamTexture, templateAmount:int) -> Dictionary:
 	var image := template.get_data()
+# warning-ignore:integer_division
+	var templatex := int(rand_range(0, image.get_width()/templateAmount))*TEMPLATE_SIZE
+	
 	return {
-		"solids" : image.get_rect(
-			Rect2(
-				Vector2(rand_range(0, image.get_width()/templateAmount), 0),
-				Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
-			)
+		"solids" : Rect2(
+			Vector2(templatex, 0),
+			Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
 		),
-		"platforms" : image.get_rect(
-			Rect2(
-				Vector2(rand_range(0, image.get_width()/templateAmount), TEMPLATE_SIZE),
-				Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
-			)
+		"platforms" : Rect2(
+			Vector2(templatex, TEMPLATE_SIZE),
+			Vector2(TEMPLATE_SIZE, TEMPLATE_SIZE)
 		)
 	}
 
