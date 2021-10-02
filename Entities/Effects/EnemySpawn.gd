243dea946cdcb4a5fe3cf8e7b6211b6937301db8
Xander <cheeseman085@gmail.com>
2021-10-02 18:54:33 -0400

    Offset spawn times by .001-.5 seconds

--- Entities/Effects/EnemySpawn.gd
+++ Entities/Effects/EnemySpawn.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 onready var enemySpawner = $EnemySpawner
+onready var anim = $AnimationPlayer
+onready var startTimer = $Timer
 
 export var enemyPool:Resource
 
 
 func _ready() -> void:
 	enemySpawner.enemyTable = enemyPool
+	startTimer.start(rand_range(0.001, .5))
+
+
+func _on_start_timer_timeout() -> void:
+	anim.play("default")
