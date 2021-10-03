30941fd389573e283b5cb66ba36a7607338976c9
Xander <cheeseman085@gmail.com>
2021-10-03 16:46:54 -0400

    Added support for custom rooms

--- World/WorldManagement/World.gd
+++ World/WorldManagement/World.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 const PADDING = 20
 
-#var tiles:TileMap
-#var altTiles:Array
-#var backgroundTiles:TileMap
 onready var player = $Props/Player
 onready var props = $Props
 onready var atmosphere = $Atmosphere
@@ -15,16 +12,12 @@ onready var mainCamMove = $Props/CameraZones/CameraMoveZone
 onready var screenTrans = $ScreenTransition
 onready var tilesContainer = $Props/Tiles
 
-export var solidPath:NodePath
-export var platformPath:NodePath
-export var zoneNumber:int = 0
-
 var worldData = preload("res://World/WorldManagement/WorldData.tres")
 
 
 func _ready():
 	AudioServer.set_bus_effect_enabled(4, 0, true)
-	
+	seed(worldData.position.x*worldData.position.y)
 	
 	for t in tilesContainer.get_children():
 		t.queue_free()
@@ -38,54 +31,120 @@ func _ready():
 	tilesContainer.add_child(platforms)
 	
 	var connections:Array = worldData.get_connected_rooms(worldData.position)
-	var room := RoomGenerator.generate(worldData.position.x+worldData.position.y, "LabTemplates.png", 13, connections)
+	var constantRooms = preload("res://World/ConstantRooms/Rooms.tres")
+	var room
 	
-	var viableEnemySpawns = [] 
-	var viableContainerSpawns = []
-	
-	var containers = [
-		preload("res://World/Props/Containers/Safe/Safe.tscn"),
-		preload("res://World/Props/Containers/Locker/Locker.tscn")
-	]
-	
-	room.lock()
-	for x in room.get_width():
-		for y in room.get_height():
-			var pixel := room.get_pixel(x, y)
-			if pixel.is_equal_approx(RoomGenerator.TILE):
-				solids.set_cell(x, y, 0)
-				solids.update_bitmask_area(Vector2(x, y))
-				
-# warning-ignore:narrowing_conversion
-				if rand_range(0, 1) < .5 and room.get_pixel(x, clamp(y+1, 0, room.get_height()-1)).is_equal_approx(RoomGenerator.EMPTY):
-					if biome.roofProps.size() > 0: add_props(biome.roofProps, x, y+1)
-# warning-ignore:narrowing_conversion
-				if rand_range(0, 1) < .1 and room.get_pixel(x, clamp(y-1, 0, room.get_height()-1)).is_equal_approx(RoomGenerator.EMPTY):
-					if biome.groundProps.size() > 0: add_props(biome.groundProps, clamp(x, 2, room.get_width()-2), y)
-					viableContainerSpawns.append(Vector2(clamp(x, 2, room.get_width()-2), y))
-			elif pixel.is_equal_approx(RoomGenerator.PLATFORM):
-				platforms.set_cell(x, y, 0)
-				platforms.update_bitmask_area(Vector2(x, y))
-			if room.get_pixel(x, y).is_equal_approx(RoomGenerator.EMPTY):
-				viableEnemySpawns.append(Vector2(x, y))
-	
-	viableContainerSpawns.shuffle()
-	for i in Globals.MAX_CONTAINERS:
-		if viableContainerSpawns.size() == 0: 
-			break
-		var spawnPos:Vector2 = viableContainerSpawns.pop_front()
-		add_props(containers, spawnPos.x, spawnPos.y-8)
-	
-	randomize()
-	viableEnemySpawns.shuffle()
-# warning-ignore:integer_division
-# warning-ignore:integer_division
-	for i in ceil((room.get_width()/Globals.TEMPLATE_SIZE)*(room.get_height()/Globals.TEMPLATE_SIZE)):
-		if viableEnemySpawns.size() == 0:
-			break
-		var spawnPos:Vector2 = viableEnemySpawns.pop_front()
-		add_props([preload("res://Entities/Effects/EnemySpawn.tscn")], spawnPos.x, spawnPos.y)
-	
+	# CONSTANT ROOMS
+	for i in constantRooms.rooms:
+		if rand_range(0, 1) < i.rarity:
+			room = i.scene.instance()
+	if room:
+		add_child(room)
+		
+		# Solids
+		var roomS:TileMap = room.solids
+		for i in roomS.get_used_cells():
+			solids.set_cellv(i, roomS.get_cellv(i))
+			solids.update_bitmask_area(i)
+		
+		# Platforms
+		var roomP:TileMap = room.platforms
+		for i in roomP.get_used_cells():
+			platforms.set_cellv(i, roomP.get_cellv(i))
+			platforms.update_bitmask_area(i)
+		
+		# Roof Props
+		var roomCeilProps:TileMap = room.ceilingProp
+		for i in roomCeilProps.get_used_cells():
+			if biome.roofProps.size() > 0: add_props(biome.roofProps, i.x, i.y)
+		
+		# Props
+		var roomPr:Node2D = room.props
+		for c in roomPr.get_children():
+			roomPr.remove_child(c)
+			add_child(c)
+		
+		# Tiling over none connected rooms
+		var size = solids.get_used_rect().end-Vector2.ONE
+		var nonconnections = [Vector2.UP, Vector2.DOWN, Vector2.RIGHT, Vector2.LEFT]
+		for i in connections: nonconnections.erase(i)
+		for i in nonconnections:
+			match i:
+				Vector2.UP:
+					for x in size.x:
+						var pos = Vector2(x, 0)
+						solids.set_cellv(pos, 0)
+						solids.update_bitmask_area(pos)
+				Vector2.DOWN:
+					for x in size.x:
+						var pos = Vector2(x, size.y)
+						solids.set_cellv(pos, 0)
+						platforms.set_cellv(pos, -1)
+						solids.update_bitmask_area(pos)
+				Vector2.RIGHT:
+					for y in size.y:
+						var pos = Vector2(size.x, y)
+						solids.set_cellv(pos, 0)
+						solids.update_bitmask_area(pos)
+				Vector2.LEFT:
+					for y in size.y:
+						var pos = Vector2(0, y)
+						solids.set_cellv(pos, 0)
+						solids.update_bitmask_area(pos)
+		room.queue_free()
+	# RANDOM ROOMS
+	else:
+		room = RoomGenerator.generate(randi(), "LabTemplates.png", 13, connections)
+		
+		var viableEnemySpawns = [] 
+		var viableContainerSpawns = []
+		
+		var containers = [
+			preload("res://World/Props/Containers/Safe/Safe.tscn"),
+			preload("res://World/Props/Containers/Locker/Locker.tscn")
+		]
+		
+		room.lock()
+		for x in room.get_width():
+			for y in room.get_height():
+				var pixel:Color = room.get_pixel(x, y)
+				# If is solid tile
+				if pixel.is_equal_approx(RoomGenerator.TILE):
+					solids.set_cell(x, y, 0)
+					solids.update_bitmask_area(Vector2(x, y))
+					
+	# warning-ignore:narrowing_conversion
+					if rand_range(0, 1) < .5 and room.get_pixel(x, clamp(y+1, 0, room.get_height()-1)).is_equal_approx(RoomGenerator.EMPTY):
+						if biome.roofProps.size() > 0: add_props(biome.roofProps, x, y+1)
+	# warning-ignore:narrowing_conversion
+					if rand_range(0, 1) < .5 and room.get_pixel(x, clamp(y-1, 0, room.get_height()-1)).is_equal_approx(RoomGenerator.EMPTY):
+						if biome.groundProps.size() > 0: add_props(biome.groundProps, clamp(x, 2, room.get_width()-2), y)
+						viableContainerSpawns.append(Vector2(clamp(x, 2, room.get_width()-2), y-1))
+				# If is platform
+				elif pixel.is_equal_approx(RoomGenerator.PLATFORM):
+					platforms.set_cell(x, y, 0)
+					platforms.update_bitmask_area(Vector2(x, y))
+				# If is empty
+				if room.get_pixel(x, y).is_equal_approx(RoomGenerator.EMPTY):
+					viableEnemySpawns.append(Vector2(x, y))
+		
+		viableContainerSpawns.shuffle()
+		for i in Globals.MAX_CONTAINERS:
+			if viableContainerSpawns.size() == 0: 
+				break
+			var spawnPos:Vector2 = viableContainerSpawns.pop_front()
+			add_props(containers, spawnPos.x, spawnPos.y-8)
+		
+		randomize()
+		viableEnemySpawns.shuffle()
+	# warning-ignore:integer_division
+	# warning-ignore:integer_division
+		for i in ceil((room.get_width()/Globals.TEMPLATE_SIZE)*(room.get_height()/Globals.TEMPLATE_SIZE)):
+			if viableEnemySpawns.size() == 0:
+				break
+			var spawnPos:Vector2 = viableEnemySpawns.pop_front()
+			add_props([preload("res://Entities/Effects/EnemySpawn.tscn")], spawnPos.x, spawnPos.y)
+		
 	var roomSize = solids.get_used_rect().end
 	create_loading_zone(Vector2(0, roomSize.y*.5)*16, Vector2(.5, roomSize.y*.5)*16, Vector2.LEFT) # Left
 	create_loading_zone(Vector2(roomSize.x, roomSize.y*.5)*16, Vector2(.5, roomSize.y*.5)*16, Vector2.RIGHT) # Right
