313f063d95e30ce2e2408b6f8e3fc9bf4ba5f214
Xander <cheeseman085@gmail.com>
2021-10-04 17:37:44 -0400

    Items are now transferred into inventory

--- Entities/Shopkeep/ShopSlot.gd
+++ Entities/Shopkeep/ShopSlot.gd
@@ -2,9 +2,16 @@ extends ColorRect
 
 var item:Dictionary
 
+var inventory = preload("res://UI/Inventory/Inventory.tres")
 
 func set_item(_item:Dictionary) -> void:
 	item = _item
 	var display = item.scene.duplicate()
 	display.position = rect_size*.5
 	add_child(display)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("use_item") and get_global_rect().has_point(get_global_mouse_position()):
+		inventory.add_item(item)
+		queue_free()
