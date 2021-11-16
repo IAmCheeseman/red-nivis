7c74a983ff253d08bbcb261a66bf3f3a24845bbe
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-11-16 12:43:27 -0500

    fixed a fix

--- Managing/Resetter.gd
+++ Managing/Resetter.gd
@@ -7,15 +7,17 @@ static func reset() -> void:
 	var inventory = preload("res://UI/Inventory/Inventory.tres")
 	var worldData = preload("res://World/WorldManagement/WorldData.tres")
 	
-	inventory.setup()
 	playerData.isDead = false
 	playerData.maxHealth = 5
 	playerData.health = playerData.maxHealth
 	playerData.maxStamina = 3
-	playerData.stamina = playerData.maxStamina
 	playerData.money = 0
 	playerData.upgrades.clear()
-	FastTravel.discoveredStations.clear()
+	
+	inventory.setup()
+	
 	worldData.rooms.clear()
+	
+	FastTravel.discoveredStations.clear()
 	
 	Engine.time_scale = 1
