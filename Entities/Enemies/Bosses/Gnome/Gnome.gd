fb4df72a916222d0428b04563273ba61ae44b3a2
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-11-20 14:07:44 -0500

    he jumps in place now

--- Entities/Enemies/Bosses/Gnome/Gnome.gd
+++ Entities/Enemies/Bosses/Gnome/Gnome.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+onready var jumpTimer = $Timers/JumpTimer
+
+export var jumpForce:int = 400
 
 var vel := Vector2.ZERO
 
@@ -7,3 +10,9 @@ func _physics_process(delta: float) -> void:
 	vel.y += Globals.GRAVITY*delta
 	
 	vel.y = move_and_slide(vel).y
+
+
+func _on_jump_timer_timeout() -> void:
+	#if !is_on_floor(): return
+	vel.y = -jumpForce
+	jumpTimer.start(rand_range(1, 2))
