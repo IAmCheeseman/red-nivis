0b062ca4db54ae6e03dca92deb6a13e54e2ed7bb
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-11-19 19:19:21 -0500

    Commented out a line of code :fingerguns:

--- World/WorldManagement/World.gd
+++ World/WorldManagement/World.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var player = $Props/Player
 onready var props = $Props
 onready var atmosphere = $Atmosphere
-onready var mistSpawner = $Props/Player/MistSpawner
+#onready var mistSpawner = $Props/Player/MistSpawner
 onready var solids = $Props/Tiles/LabSolids
 onready var platforms = $Props/Tiles/OneWayPlatforms
 onready var background = $Props/Tiles/LabBackground
