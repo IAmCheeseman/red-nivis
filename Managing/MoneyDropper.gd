c1e1e53ca1e8f055caea5a7ffc783271e87b9f81
Xander <cheeseman085@gmail.com>
2021-12-11 12:48:54 -0500

    Nerfed shotgun

--- Managing/MoneyDropper.gd
+++ Managing/MoneyDropper.gd
@@ -4,10 +4,14 @@ class_name MoneyDropper
 export var moneyRange:Vector2 = Vector2(1, 4)
 export var valueRange:Vector2 = Vector2(1, 2)
 
+var used = false
+
 var money = preload("res://Items/Money/Money.tscn")
 
 
 func drop_money():
+	if used: return
+	used = true
 	for m in rand_range(moneyRange.x, moneyRange.y):
 		var newMoney = money.instance()
 		newMoney.global_position = global_position
