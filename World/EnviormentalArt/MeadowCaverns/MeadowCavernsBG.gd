981c213c161f152e2ab79c51ff1ac6624c87fe58
Xander <cheeseman085@gmail.com>
2021-12-27 13:54:01 -0500

    Removed parallax

--- World/EnviormentalArt/MeadowCaverns/MeadowCavernsBG.gd
+++
@@ -1,7 +0,0 @@
-extends TileMap
-
-
-func _process(delta: float) -> void:
-	var pos = GameManager.currentCamera.global_position
-	pos += GameManager.currentCamera.offset
-	global_position = pos * 0.025
