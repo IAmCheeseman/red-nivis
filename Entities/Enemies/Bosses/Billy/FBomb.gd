ed72268f61be2cbe22f569f80dcbc02aec669daf
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-02-03 14:17:29 -0500

    Billy throws the FBomb, instead of dropping it

--- Entities/Enemies/Bosses/Billy/FBomb.gd
+++ Entities/Enemies/Bosses/Billy/FBomb.gd
@@ -5,7 +5,11 @@ onready var rc = $RayCast2D
 onready var anim = $AnimationPlayer
 onready var collider = $Hitbox/CollisionShape2D
 
-var vel := Vector2(0, -200)
+var vel := Vector2.ZERO
+
+func _ready() -> void:
+	vel = global_position.direction_to(GameManager.player.global_position) * 300
+	vel.y -= 200
 
 func _physics_process(delta: float) -> void:
 	sprite.rotation += delta * 2
@@ -20,5 +24,5 @@ func _physics_process(delta: float) -> void:
 func _draw() -> void:
 	if !sprite.visible: return
 	var radius = collider.shape.radius
+	draw_circle(Vector2.ZERO, radius + 8, Color(1, 0, 0, .25))
 	draw_arc(Vector2.ZERO, radius + 8, 0, PI * 2, 22, Color.red, 2)
-#	draw_circle(Vector2.ZERO, radius, Color.red)
