26ba6ca3cf44736913e828fcc25d3e741bb9925c
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-02-03 08:14:21 -0500

    Removed useless files

--- Managing/Init.gd
+++ Managing/Init.gd
@@ -7,8 +7,6 @@ const WORLD = "res://World/WorldManagement/World.tscn"
 func _ready():
 	randomize()
 	
-	if OS.has_feature("standalone"):
-		OS.set_window_always_on_top(false)
 	OS.window_fullscreen = Settings.fullscreen
 	var target = STARTING_AREA if GameManager.load_run() != OK else WORLD
 	var _discard = get_tree().change_scene(target)
