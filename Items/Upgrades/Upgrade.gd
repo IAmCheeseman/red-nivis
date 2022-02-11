408ffee016b2c8dc0f487607164a743509769dc6
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-02-11 08:22:40 -0500

    Added hologram shader

--- Items/Upgrades/Upgrade.gd
+++ Items/Upgrades/Upgrade.gd
@@ -3,6 +3,7 @@ class_name Upgrade
 
 export var icon:StreamTexture = preload("res://Items/Upgrades/DoubleJump/DoubleJumpUpgrade.png")
 export var miniIcon:StreamTexture = preload("res://Items/Upgrades/DoubleJump/MiniIcon.png")
+export var hologramSprite:StreamTexture
 export var name:String = "Ability"
 export var description:String = "Does nothing yet."
 export var howTo:String = "[Empty how-to]"
