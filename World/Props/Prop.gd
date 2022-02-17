ccb1f333bcbe4fa2c7594ba20253ab75d4acf97f
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-02-17 15:54:55 -0500

    New props for caves

--- World/Props/Prop.gd
+++ World/Props/Prop.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 onready var tileCheckers = $TileCheckers
+onready var sprite = $Sprite
 
 func _ready() -> void:
 	for i in tileCheckers.get_children():
 		if !i.is_colliding(): queue_free()
 	tileCheckers.queue_free()
+	sprite.flip_h = Utils.coin_flip()
