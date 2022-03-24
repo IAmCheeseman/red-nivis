dc49369a686117a9f8b988cde734e4f55f45ae48
Xander Swan <cheeseman085@gmail.com>
2022-03-24 19:23:58 -0400

    B L O O D

--- Entities/Enemies/Assets/BloodManager.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var blood = preload("res://Enemies/Blood.tscn")
-
-func emit_blood(dir : Vector2):
-	var newBlood = blood.instance()
-	newBlood.rotation = dir.angle()
-	add_child(newBlood)
