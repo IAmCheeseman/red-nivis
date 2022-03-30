03902bf0efc2163da39db7ab8e96eccb2414e453
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-03-30 10:01:33 -0400

    Jumping on enemies with the boots do damage now

--- Items/Upgrades/DoubleJump/DoubleJump.gd
+++ Items/Upgrades/DoubleJump/DoubleJump.gd
@@ -3,11 +3,25 @@ extends Node
 var player:Node2D
 
 var hasJumped = false
+var hitbox: Area2D
+
+
+func _ready() -> void:
+	hitbox = preload("res://Items/Upgrades/DoubleJump/BootHitbox.tscn").instance()
+	hitbox.connect("hit_object", self, "bounce")
+	hitbox.damage = 30
+	add_child(hitbox)
+
+
+func bounce(_a=null) -> void:
+	if !player: return
+	player.vel.y = -150
 
 
 func _process(_delta: float) -> void:
 	if !player: return
-	
+	if hitbox: hitbox.position = player.position
+
 	var isNotJumping = player.vel.y > 0
 	if player.is_on_floor()\
 	or player.state == player.states.WALLSLIDE\
