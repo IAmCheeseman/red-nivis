141340e007f0a38e45aefe5a821aacbba4d2beff
Xander Swan <cheeseman085@gmail.com>
2022-04-04 20:04:03 -0400

    FISH GOBLIN!!!!!!

---
+++ Entities/Enemies/Goblins/Goblin.gd
@@ -0,0 +1,96 @@
+extends KinematicBody2D
+
+enum states { IDLE, MOVE, ATTACK }
+
+onready var sprite = $Sprite
+onready var anim = $AnimationPlayer
+
+onready var playerDetection = $Collisions/PlayerDetection
+onready var wallDetection = $Collisions/Wall
+
+onready var stateChangeTimer = $Timers/StateChange
+
+export var targetRange := 150
+export var frict := 5.0
+export var accel := 7.0
+export var speed := 70.0
+export var lookAtSwim := true
+
+var player: Node2D
+var state = states.IDLE
+
+var vel := Vector2.ZERO
+var targetPos := global_position
+
+
+func _ready() -> void:
+	assert(anim.has_animation("Idle"))
+	assert(anim.has_animation("Swim"))
+
+
+func _process(delta: float) -> void:
+	if !player:
+		player = playerDetection.get_player()
+		anim.play("Idle")
+	else:
+		match state:
+			states.IDLE:
+				idle_state(delta)
+			states.MOVE:
+				move_state(delta)
+			states.ATTACK:
+				attack_state(delta)
+		
+		# Bouncing
+		wallDetection.cast_to = vel.normalized() * (sprite.texture.get_width() / 3)
+		wallDetection.force_raycast_update()
+		if wallDetection.is_colliding():
+			vel = vel.bounce(wallDetection.get_collision_normal())
+		
+		vel = move_and_slide(vel)
+
+
+func idle_state(delta: float) -> void:
+	vel = vel.move_toward(Vector2.ZERO, frict * delta)
+	
+	anim.play("Idle")
+
+
+func move_state(delta: float) -> void:
+	vel = vel.move_toward(
+		global_position.direction_to(targetPos) * speed,
+		accel * delta
+	)
+	
+	if lookAtSwim:
+		var currentRot = sprite.rotation - PI / 2
+		sprite.look_at(targetPos)
+		var targetLook = sprite.rotation
+		sprite.rotation = lerp(currentRot, targetLook, (accel / 5) * delta) + PI / 2
+	
+	sprite.flip_h = vel.x > 0
+	
+	anim.play("Swim")
+
+
+func attack_state(delta: float) -> void:
+	pass
+
+
+func get_target() -> void:
+	if !player: return
+	targetPos =\
+		player.global_position + Vector2(rand_range(0, targetRange), 0)\
+								.rotated(rand_range(0, PI * 2))
+
+
+func toggle_state() -> void:
+	if state == states.ATTACK: return
+	
+	if state == states.IDLE:
+		state = states.MOVE
+	else:
+		state = states.IDLE
+	
+	stateChangeTimer.start(rand_range(1, 4))
+	
---
+++ Entities/Enemies/Goblins/ShockwaveGoblin/Attack.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+const BULLET = preload("res://Entities/Enemies/EnemyBullet/EnemyBullet.tscn")
+
+onready var sprite = $Sprite
+onready var shootTimer = $ShootTimer
+onready var bounceRay = $BounceRay
+
+export var recoil := 140.0 / 5
+
+var player: Node2D
+
+func _process(delta: float) -> void:
+	if !player:
+		player = owner.player
+		return
+	
+	sprite.look_at(player.global_position)
+	sprite.flip_v = player.global_position.x < global_position.x
+
+
+func shoot() -> void:
+	shootTimer.start(rand_range(3, 5))
+	if !player: return
+	
+	var pointDir = Vector2.RIGHT.rotated(sprite.rotation)
+	
+	owner.vel = -pointDir * recoil
+	
+	for i in 8:
+		create_bullet(
+			null,
+			70 + rand_range(-30, 0),
+			pointDir.rotated(deg2rad(rand_range(-35, 35)))
+		)
+
+
+func create_bullet(bullet=null, speed:int=0, dir:Vector2=Vector2.ZERO, timesLeft:int=3) -> void:
+	if timesLeft <= 0: return
+	var realDir = dir
+	var pos = global_position
+	if dir == Vector2.ZERO:
+		bounceRay.global_position = bullet.global_position
+		bounceRay.cast_to = bullet.direction * 32
+		bounceRay.force_raycast_update()
+		
+		realDir = bullet.direction.bounce(bounceRay.get_collision_normal())
+		pos = bullet.global_position + (realDir * 8)
+	
+	var newBullet = BULLET.instance()
+	newBullet.global_position = pos
+	newBullet.direction = realDir
+	newBullet.speed = speed
+	
+	newBullet.connect(
+		"hitCollision", self, "create_bullet",
+		[speed, Vector2.ZERO, timesLeft-1]
+	)
+	
+	GameManager.spawnManager.spawn_object(newBullet)
