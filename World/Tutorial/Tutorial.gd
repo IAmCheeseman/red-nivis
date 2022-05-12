59eef8512bd539ec76fc4b3b00f29438b7547e49
xander-swan <cheeseman085@gmail.com>
2022-05-12 18:36:24 -0400

    Fixed bug in tutoria

--- World/Tutorial/Tutorial.gd
+++ World/Tutorial/Tutorial.gd
@@ -5,6 +5,7 @@ onready var parrySignAnim = $Props/ParrySign/AnimationPlayer2
 onready var suicideSign = $Props/DeathSign
 onready var shooters = $Props/Shooters
 
+onready var snowRemoteTransform = $Player/ParticleRmtTrsfm
 
 func _ready() -> void:
 	GameManager.inGUI = false
@@ -17,16 +18,18 @@ func _ready() -> void:
 
 func _on_load_area() -> void:
 	GameManager.emit_signal("screenshake", 10, 4, .025, .333)
+	if is_instance_valid(snowRemoteTransform): snowRemoteTransform.queue_free()
+	
 	yield(get_tree().create_timer(1.5), "timeout")
 	suicideSign.dialog.start_dialog("Jumped")
 	suicideSign.connect("dialog_finished", self, "start_load")
+
 
 func start_load() -> void:
 	yield(get_tree().create_timer(.5), "timeout")
 	$ScreenTransition.out()
 	var timer = Timer.new()
 	
-
 	timer.connect("timeout", self, "load_world")
 	add_child(timer)
 	timer.start(.3)
