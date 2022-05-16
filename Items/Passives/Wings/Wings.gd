c1d63e32db76446f40bc510f42054c34686eec22
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-05-16 15:45:41 -0400

    Changed wings to a jetpack 🙀

--- Items/Passives/Wings/Wings.gd
+++ Items/Passives/Wings/Wings.gd
@@ -4,19 +4,19 @@ extends Node2D
 const PLAYER = preload("res://Entities/Player/Player.tres")
 
 onready var sprite = $Sprite
-onready var anim = $AnimationPlayer
+onready var particles = $Sprite/Fire
 
 
 func _process(delta: float) -> void:
 	var player = PLAYER.playerObject
 	
-	player.vel.y -= (Globals.GRAVITY * delta) / 4
-	
-	if player.is_grounded():
-		anim.play("Idle")
-	else:
-		anim.play("Flap")
-		player.scaleHelper.scale = player.scaleHelper.scale / player.scaleHelper.scale
+	particles.emitting = false
+	if !player.is_grounded() and Input.is_action_pressed("jump") and player.vel.y > -PLAYER.jumpForce * .1:
+			player.vel.y -= (Globals.GRAVITY * delta) * .95
+			player.vel.y = clamp(player.vel.y, -INF, Globals.GRAVITY * .05)
+			particles.emitting = true
+			
+			player.scaleHelper.scale = player.scaleHelper.scale / player.scaleHelper.scale
 	
 	sprite.scale = player.scaleHelper.scale * player.sprite.scale
 	global_position = player.global_position
