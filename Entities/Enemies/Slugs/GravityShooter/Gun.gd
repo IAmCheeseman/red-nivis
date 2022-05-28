3bb7a738dfe1704802d554b97e0b22d6d3815425
xander-swan <cheeseman085@gmail.com>
2022-05-27 20:32:03 -0400

    SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY SLUGGY

--- Entities/Enemies/Slugs/GravityShooter/Gun.gd
+++ Entities/Enemies/Slugs/GravityShooter/Gun.gd
@@ -9,31 +9,29 @@ var bullet = preload("res://Entities/Enemies/EnemyBullet/Falling/FallingEnemyBul
 
 var player: Node2D
 
+var attacking := false
+var shots := 0
 
-func _process(_delta: float) -> void:
+
+func _process(delta: float) -> void:
 	if !player: return
-#	var add = -32 if player.global_position.x < global_position.x else 32
-#	look_at(Vector2(global_position.x + add, player.global_position.y - 64))
 	
 	look_at(player.global_position - Vector2(0, 128))
 	
 	sprite.flip_v = Vector2.RIGHT.rotated(rotation).x < 0
-	sprite.material.set_shader_param("isOn", int(cooldown.time_left < .6))
-	sprite.scale.x = clamp(cooldown.time_left / cooldown.wait_time, .5, INF)
-	sprite.scale.y = (1 - sprite.scale.x) + 1
+	
+	sprite.scale = sprite.scale.move_toward(Vector2.ONE, 5 * delta)
+#	sprite.material.set_shader_param("isOn", int(cooldown.time_left < .6))
+#	sprite.scale.x = clamp(cooldown.time_left / cooldown.wait_time, .5, INF)
+#	sprite.scale.y = (1 - sprite.scale.x) + 1
 
 
 func shoot() -> void:
-	cooldown.start(cooldown.wait_time + rand_range(-.4, .4))
-	if !player: return
-	if GameManager.attacks_capped():
-		attackTimer.start()
-		return
+	if !player or !attacking: return
 	
 	owner.vel = -Vector2.RIGHT.rotated(rotation)*250
 	
-	
-	var dir = Vector2.RIGHT.rotated(rotation)
+	var dir = Vector2.RIGHT.rotated(rotation + deg2rad(rand_range(-12, 12)) )
 	
 	var newBullet = bullet.instance()
 	newBullet.direction = dir
@@ -46,16 +44,13 @@ func shoot() -> void:
 	yield(TempTimer.idle_frame(self), "timeout")
 	newBullet.set_texture(preload("res://Entities/Enemies/Slugs/GravityShooter/Slime.png"))
 	
-	GameManager.add_attacking_enemy(self)
-	randomize()
-	attackTimer.start()
+	sprite.scale = Vector2(1.5, 0.5)
+	
+	shots += 1
+	if shots >= 3:
+		shots = 0
+		attacking = false
 
 
-func _on_attack_timer_timeout() -> void:
-	if GameManager.enemy_is_attacking(self):
-		GameManager.remove_attacking_enemy(self)
-
-
-func _on_tree_exiting() -> void:
-	if GameManager.enemy_is_attacking(self):
-		GameManager.remove_attacking_enemy(self)
+func _on_cooldown_timeout() -> void:
+	attacking = true
