0f90e7c2950bd1a4919ab91b965190b7338a0472
xander-swan <cheeseman085@gmail.com>
2022-07-09 11:39:44 -0400

    Added matthew to the world

--- Items/QuestItems/QuestLog.gd
+++ Items/QuestItems/QuestLog.gd
@@ -6,5 +6,5 @@ onready var quests = find_node("Quests")
 func _ready() -> void:
 	for i in QuestManager.activeQuests:
 		var label = Label.new()
-		label.text = i.name()
+		label.text = i.name
 		quests.add_child(label)
