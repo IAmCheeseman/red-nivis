5b93b6832b7d7211cf5b04d73ce8c4ab4505fa43
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-08-08 15:14:36 -0400

    Splody guy

--- Entities/Enemies/Explosion/Explosion.gd
+++ Entities/Enemies/Explosion/Explosion.gd
@@ -12,14 +12,14 @@ func _ready() -> void:
 	eHB.owner.damage = damage
 	if startSize != -1: set_size(startSize)
 
-func set_size(size: int) -> void:
+func set_size(size: int, playerSize=null) -> void:
 	yield(TempTimer.idle_frame(self), "timeout")
 	particles.process_material = particles.process_material.duplicate()
 	particles.process_material.emission_sphere_radius = size
 	eHB.shape.radius = size / 2.0
-	pHB.shape.radius = (size / 2.0) * 0.90
+	pHB.shape.radius = (size / 2.0) * 0.90 if playerSize == null else playerSize
 	charAdder.startDist = size
 	charAdder.endDist = size
 	
-	particles.amount = size / 10.0
+	particles.amount = size / 1.0
 	particles.restart()
