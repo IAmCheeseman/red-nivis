791b060eac07158f7b0bf927385b3c0094a4f878
iamcheeseman <xanderswan32@gmail.com>
2022-09-30 20:08:18 -0400

    Melee charging

--- Items/Weapons/GunSwing.gd
+++ Items/Weapons/GunSwing.gd
@@ -4,13 +4,14 @@ onready var hitbox = $Hitbox
 onready var swishSFX = $SwishSFX
 
 var reflectDir:Vector2
+var canReflect := true
 
 func _ready() -> void:
 	swishSFX.play()
 
 func _on_area_entered(area: Area2D) -> void:
 	var body = area.get_parent()
-	if body.is_in_group("EnemyBullet"):
+	if body.is_in_group("EnemyBullet") and canReflect:
 		body.direction = reflectDir
 		body.hitbox.collision_mask = 4
 		body.hitbox.damage = hitbox.damage*1.5
