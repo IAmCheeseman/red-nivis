8f7f7cc52bc1cbffc829e88abd8b8f6d9c74662a
Xander Swan <xanderswan32@gmail.com>
2023-03-07 15:06:38 -0500

    Fixed mr. slug man :smiling_face_with_3_hearts:

--- Entities/NPC/TheGuy/TheGuy.gd
+++ Entities/NPC/TheGuy/TheGuy.gd
@@ -16,23 +16,28 @@ func _process(delta: float) -> void:
 	sprite.scale = sprite.scale.move_toward(Vector2.ONE, 2 * delta)
 	
 	if rc.is_colliding():
+		# On ground
 		sprite.frame = 0
 		sprite.rotation = 0
 		vel.x = lerp(vel.x, 0, 5 * delta)
 	else:
+		# Stretch
 		sprite.frame = 1
 		sprite.scale.y = clamp((1 / vel.length()) * 275, .5, 1.5)
 		sprite.scale.x = (1-sprite.scale.y) + 1 
 		sprite.rotation = vel.angle()
 		
+		# Bounce
 		rc.cast_to = vel.normalized() * 8
 		rc.force_raycast_update()
 		if rc.is_colliding():
 			vel = vel.bounce(rc.get_collision_normal()) * .8
 			if get_tree().paused: landSFX.play()
-	rc.cast_to = Vector2(0,6)
+	# Casting to the ground
+	rc.cast_to = Vector2(0, 12)
 	rc.force_raycast_update()
 	
+	# if we're on the ground, clicking on the slug, emit love particles
 	if rc.is_colliding() and !prevGrounded or\
 	(Input.is_action_just_pressed("use_item") and mouseClose):
 		sprite.scale = Vector2(1.2, .8)
@@ -42,6 +47,7 @@ func _process(delta: float) -> void:
 		add_child(newLove)
 	
 	if get_tree().paused:
+		# Dragging slug
 		if (Input.is_action_pressed("use_item")\
 		and (mouseClose and !grabbed))\
 		or grabbed:
@@ -51,6 +57,7 @@ func _process(delta: float) -> void:
 				1000 * delta
 			)
 			grabbed = true
+		# Chucking slug
 		if Input.is_action_just_released("use_item"):
 			vel *= 2.5
 			grabbed = false
